108633f184e1b4bb10e3cb8ddd121e07a3020e87
sarath <sasidharansharath13@gmail.com>
2025-08-09 18:01:47 +0530

    script_added

--- playwright.config.js
+++ playwright.config.js
@@ -26,6 +26,8 @@ export default defineConfig({
   reporter: [["html"], ["allure-playwright"]],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
+    screenshot: "on",
+    video: "on",
     /* Base URL to use in actions like `await page.goto('/')`. */
     // baseURL: 'http://localhost:3000',
 
