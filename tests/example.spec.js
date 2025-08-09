fe8cd4788b9b2f02b8507f4730013d7e93f55679
sarath <sasidharansharath13@gmail.com>
2025-08-09 17:55:44 +0530

    script_added

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -26,3 +26,24 @@ test("frames handling", async ({ page }) => {
   const frame = page.frameLocator('frame[src*="frame_3.html"]');
   await frame.locator('input[name*="mytext3"]').fill("great");
 });
+test("Auto Sugesstion dropdown - flipkart", async ({ page }) => {
+  await page.goto("https://www.flipkart.com/");
+  const productName = "watch";
+  const searchBox = page.locator("input.Pke_EE");
+  await searchBox.waitFor(); // wait for input to be ready
+  await searchBox.pressSequentially(productName, { delay: 200 });
+  const suggestions = page.locator("ul._1sFryS li"); // Updated selector
+  await suggestions.first().waitFor({ timeout: 5000 });
+
+  const count = await suggestions.count();
+  console.log("Suggestion count:", count);
+
+  for (let i = 0; i < count; i++) {
+    const suggestionText = await suggestions.nth(i).locator("a").textContent();
+    console.log(suggestionText, "===");
+    if (suggestionText.includes("wild stone perfume")) {
+      await suggestions.nth(i).locator("a").click();
+      break;
+    }
+  }
+});
